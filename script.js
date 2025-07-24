77011725d497a30f2f23d54d9cac03c446f212c1
finnensley <finniganensley@gmail.com>
2025-07-24 13:35:28 -0600

    updated error handling in weather

--- script.js
+++ script.js
@@ -171,8 +171,8 @@ function renderNoteCards() {
 };
 
 //Weather
-const localApiURL = "https://api.openweathermap.org/data/2.5/weather?q=Colorado,us&APPID=9818f918e9d02e8a934463e2e7602786"
-const api = "9818f918e9d02e8a934463e2e7602786";
+// const localApiURL = "https://api.openweathermap.org/data/2.5/weather?q=Colorado,us&APPID=9818f918e9d02e8a934463e2e7602786"
+const apiKey = "9818f918e9d02e8a934463e2e7602786";
      
 const btn = document.getElementById("weatherBtn");
   btn.addEventListener("click", () => {
@@ -185,7 +185,7 @@ const btn = document.getElementById("weatherBtn");
 });
 
  function fetchWeather(city) {
-    const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api}&units=metric`;
+    const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
 
     fetch(apiURL)
       .then(response => {
@@ -198,16 +198,16 @@ const btn = document.getElementById("weatherBtn");
       .then(data => displayWeather(data))
       .catch(error => displayError(error.message));
       
-  }
+  };
 
   function displayWeather(data) {
     document.getElementById("error-message").textContent = "";
     document.getElementById("city-name").textContent = `Weather in ${data.name}`;
-    document.getElementById("temperature").textContent = `Current Temperature: ${data.main.temp} C`;
-    document.getElementById("feels-like").textContent = `Feels like: ${data.main.feels_like} C`;
+    document.getElementById("temperature").textContent = `Current Temperature: ${Math.floor(data.main.temp)}°F`;
+    document.getElementById("feels-like").textContent = `Feels like: ${Math.floor(data.main.feels_like)}°F`;
     document.getElementById("description").textContent = `Condition: ${data.weather[0].description}`;
     document.getElementById("humidity").textContent = `Humidity: ${data.main.humidity}%`;
-  }
+  };
 
   function displayError(message) {
     document.getElementById("error-message").textContent = message;
@@ -216,11 +216,9 @@ const btn = document.getElementById("weatherBtn");
     document.getElementById("feels-like").textContent = "";
     document.getElementById("description").textContent = "";
     document.getElementById("humidity").textContent = "";
-  }
+  };
 //Challenges
-// F as well as C
 //add Weather Icons: Use weather icons from the API response.
-//add Styling
 //add forecast: show a 5-day weather forecast
 
 //add Geolocation: Automatically detect the user's location and display the weather.
@@ -230,38 +228,24 @@ const btn = document.getElementById("weatherBtn");
     const longitude = position.coords.longitude;
     //use these coordinates to fetch weather data
     fetchWeatherByCoordinates(latitude, longitude);
- }
+ };
 
   function errorCallback(error) {
-      switch(error.code) {
-         case error.PERMISSION_DENIED:
-             console.error("User denied the request for Geolocation.");
-            // Optionally, prompt the user to manually enter a location
-              break;
-         case error.POSITION_UNAVAILABLE:
-             console.error("Location information is unavailable.");
-              break;
-          case error.TIMEOUT:
-              console.error("The request to get user location timed out.");
-              break;
-          case error.UNKNOWN_ERROR:
-              console.error("An unknown error occurred.");
-              break;
-      }
-  }
-
+    console.error("Unable to get your location.");
+    displayError("Unable to get your location.");
+  };
 
   async function fetchWeatherByCoordinates(latitude, longitude) {
     try {
-         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${api}`);
+         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${apiKey}&units=imperial`);
          const weather = await response.json();
          displayWeather(weather);
-         //console.log("weather = ", weather);
+         console.log("weather = ", weather);
     } catch (error) {
          console.error("Error fetching weather data:", error);
          displayError("Could not fetch local weather");
     };
-  }
+  };
 
     
    const localBtn = document.getElementById("localBtn");
@@ -269,4 +253,4 @@ const btn = document.getElementById("weatherBtn");
     localBtn.addEventListener("click", () => {
         navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
    });
-  }
+  };
